48a3382425e38516a4465a3d04caad8eb3460ffd
Imane <imanebouzeguella@gmail.com>
2024-10-05 16:16:32 +0100

    initialized rep.

---
+++ backend/app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const cors = require('cors');
+const app = express();
+const port = 3001;
+const corsOptions = {
+  origin: 'http://localhost:3000', // Allow requests from this origin
+  methods: ['GET', 'POST', 'PUT' , 'DELETE'], // Allow specified HTTP methods
+};
+app.use(cors(corsOptions));
+const publicDirectory = path.join(__dirname, '/public'); 
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+app.set('view engine', 'hbs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static(publicDirectory));
+const getAuthRoutes = require('./routes/auth');
+
+
+// defining routes
+app.use('/auth', getAuthRoutes);
+
+
+app.use((req, res, next) => {
+  res.status(404).render("404");
+});
+
+
+app.listen(port, (err) => {
+  if (err) console.log(`Error listening on port ${port}`);
+  else  console.log(`Running on port ${port}`);
+});
